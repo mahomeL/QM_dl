69541482cc513c9300cb1de5008a1c860d7317cf
mahomeL <11156539@qq.com>
2017-03-12 14:20:19 +0800

    add gauge and predict result logging

--- clothes/wx_test.py
+++ clothes/wx_test.py
@@ -4,7 +4,6 @@
 
 import wx
 
-
 class MyFrame(wx.Frame):
     def __init__(self,parent=None,title ='my title',size=(600,400)):
         wx.Frame.__init__(self,parent=parent,
@@ -12,7 +11,7 @@ class MyFrame(wx.Frame):
         self.Centre()
         self.panel = wx.Panel(self,-1)
 
-        ##text,button
+        '''text,button'''
         self.text_intro = wx.StaticText(self.panel,-1,'Clothes-classfier by deep learning',pos=(180,0))
         # self.text_intro.SetBackgroundColour('green')
         self.text_intro.SetFont(wx.Font(26,wx.SWISS,wx.NORMAL,wx.BOLD))
@@ -28,8 +27,21 @@ class MyFrame(wx.Frame):
         self.load_pic = wx.DirPickerCtrl(self.panel,4,path='path_to_pic',size=(400,-1),message='abc')
         self.output_res = wx.DirPickerCtrl(self.panel,4, path='output_result', size=(400, -1), message='abc111')
 
+        self.gauge_training = wx.Gauge(self.panel,-1,size=(400,-1))
 
-        ##layout
+        self.logging_output = wx.TextCtrl(self.panel,-1,style =wx.TE_MULTILINE)
+        wx.CallAfter(self.dologging)
+
+        '''event'''
+        self.load_model_archi.Bind(wx.EVT_FILEPICKER_CHANGED,self.event_getpath,self.load_model_archi)
+
+        self.load_pic.Bind(wx.EVT_DIRPICKER_CHANGED,self.event_getpath,self.load_pic)
+
+
+
+
+
+        """layout"""
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         main_sizer.Add(self.text_intro,0,flag=wx.CENTER)
         main_sizer.Add(self.text_line,0,flag=wx.EXPAND)
@@ -42,11 +54,20 @@ class MyFrame(wx.Frame):
         left_sizer.Add(self.sizer_model_staticbox('Input-pic Output-result', text=[self.text_load_pic, self.text_res_output],
                                                   item=[self.load_pic, self.output_res]),1)
 
-
+        left_sizer.Add(self.gauge_training,1)
+        left_sizer.Add(self.logging_output,1,flag=wx.EXPAND)
         main_sizer.Add(left_sizer,1,flag=wx.LEFT)
 
         self.panel.SetSizer(main_sizer)
         main_sizer.Fit(self)
+        wx.Log.SetActiveTarget(wx.LogTextCtrl(self.logging_output))
+
+
+    def event_getpath(self,event):
+        '''get browse file/dir path'''
+        print(event.GetPath())
+        event.Skip()
+
 
     def sizer_model_staticbox(self,boxlabel,text,item):
         '''build staticbox for model and pic'''
@@ -68,15 +89,17 @@ class MyFrame(wx.Frame):
         model_sizer.Add(item,0,flag = flag)
         return model_sizer
 
-
-
-
+    def dologging(self):
+        '''print output predict result on panel'''
+        print ('do logging....')
+        wx.LogMessage('text message')
+        wx.CallLater(3000,self.dologging)
 
 
 
 class GetClothesApp(wx.App):
     def OnInit(self):
-        frame = MyFrame(title='qingmu clothes')
+        frame = MyFrame(title='QingMu clothes V1.0')
         frame.Show()
         return True
 
